37bf62b7a47ddddfdc4ef532eb7904c06bebb470
Aafiya Javed <aafiyajaved22@gmail.com>
2025-02-02 19:29:13 +0530

    final

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -6,10 +8,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("dotenv").config();
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mydb', { useNewUrlParser: true, useUnifiedTopology: true });
-
+mongoose.connect('mongodb://localhost:27017/mydb', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+});
 
 
 // Define User Schema
@@ -24,29 +26,47 @@ const User = mongoose.model('User', userSchema);
 
 // Display Users
 app.get("/", async (req, res) => {
-    const users = await User.find();
-    res.render("home", { data: users });
+    try {
+        const users = await User.find();
+        res.render("home", { data: users });
+    } catch (err) {
+        res.status(500).send("Error fetching users");
+    }
 });
 
 // Add User
 app.post("/", async (req, res) => {
-    const newUser = new User(req.body);
-    await newUser.save();
-    res.redirect("/");
+    try {
+        const newUser = new User(req.body);
+        await newUser.save();
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send("Error adding user");
+    }
 });
 
 // Update User
 app.post("/update", async (req, res) => {
-    await User.updateOne({ userUniqueId: req.body.userUniqueId }, req.body);
-    res.redirect("/");
+    try {
+        await User.updateOne({ userUniqueId: req.body.userUniqueId }, req.body);
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send("Error updating user");
+    }
 });
 
 // Delete User
 app.post("/delete", async (req, res) => {
-    await User.deleteOne({ userUniqueId: req.body.userUniqueId });
-    res.redirect("/");
+    try {
+        await User.deleteOne({ userUniqueId: req.body.userUniqueId });
+        res.redirect("/");
+    } catch (err) {
+        res.status(500).send("Error deleting user");
+    }
 });
 
-app.listen(3000, () => {
-    console.log("App running on http://localhost:3000");
+// Start Server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App running on http://localhost:${PORT}`);
 });
